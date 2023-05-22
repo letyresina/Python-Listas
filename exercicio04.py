39d9ee2834a98a19ce860bda73f387d4430c7e06
letyresina <leticiaresina@gmail.com>
2023-05-22 09:47:35 -0300

    subindo comentário no exercício 4

---
+++ exercicio04.py
@@ -0,0 +1,5 @@
+'''
+    Solicite os nomes e as idades de 10 pessoas. Armazene os nomes em uma lista e as idades em 
+    outra lista. Na sequência, exiba os nomes de todas as pessoas que possuem idade maior ou 
+    igual a 18 anos.
+'''

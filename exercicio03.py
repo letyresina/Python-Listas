94d944f71d7a26be48779999b465ef678a0cab81
letyresina <leticiaresina@gmail.com>
2023-05-22 09:42:10 -0300

    Subindo comentário no exercício 3

---
+++ exercicio03.py
@@ -0,0 +1,7 @@
+'''
+    Preencha uma lista com 20 números inteiros aleatórios sorteados entre 1 e 50 e exiba: 
+    a. a lista com todos os itens armazenados. 
+    b. o somatório de todos os números contidos na lista. 
+    c. o maior número da lista. 
+    d. o menor número da lista.
+'''
